15d9da135e13da635193c1537990e1f8d332e38b
Joe O <j0liverko@gmail.com>
2020-08-04 16:48:22 -0500

    spelling correction

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+    
+    return(
+        <section>
+            <section>
+                <h5>Hello my name is Joseph Oliver </h5>
+                <p>Im a person that likes to learn new languages, frameworks and new challenges. <br/>
+                    </p>
+            </section>
+
+            <section>
+
+            </section>
+        </section>
+    )
+}
