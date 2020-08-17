c4c1ed55160606c8effd5ec37d83f03d7ea28b9b
Joe O <j0liverko@gmail.com>
2020-08-16 20:18:44 -0500

    added props for skill card

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const SkillCard = () => {
+const SkillCard = (props) => {
     
     return(
         <section>
-
+            <img src={props.skill} alt=""/>
+            <h6>{props.skillName}</h6>
         </section>
     )
 }
