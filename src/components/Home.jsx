55fbd1d88269783cdc392cc01f6a85bc0f8582ff
Joe O <j0liverko@gmail.com>
2020-08-17 10:50:59 -0500

    adding Skillcard css file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './Home.css'
 
-//icons 
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNode } from 'react-icons/fa';
-
 //components
 import SkillCard from './SkillCard'
 
 const Home = () => {
 
-    const skillData = [{"Html5": FaHtml5}, {"Css3": FaCss3}]
     return(
         <section className='HomeTextContainer'>
             <section>
@@ -17,16 +13,8 @@ const Home = () => {
                 <p>Im a person that likes to learn new languages, frameworks and new challenges. Im always consuming  <br/>
                     </p>
             </section>
-                {/* <p>
-                    talk about why i like tech etc adfkajdksafsa daf sa sda
-                </p>
-                links here to message? */}
-            <section>
-                <h6>bleh</h6>
-                {skillData.forEach((skills) => {
-                    <SkillCard skill={skills.value} skillName={skill.key}/>
-                })}
-            </section>
+            <SkillCard />
+            
         </section>
     )
 }
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import './SkillCard.css'
+//icons 
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNode } from 'react-icons/fa';
 
-const SkillCard = (props) => {
+const SkillCard = () => {
     
     return(
+        <section className='skills'>
+        <h5>Skills</h5>
         <section>
-            <img src={props.skill} alt=""/>
-            <h6>{props.skillName}</h6>
+            <FaHtml5 />
+            <h6>Html5</h6>
+        </section>   
+        <section>
+            <FaCss3 />
+            <h6>Css3</h6>
         </section>
+        <section>
+            <FaJs />
+            <h6>JavaScript</h6>
+        </section>
+        <section>
+            <FaReact />
+            <h6>React</h6>
+        </section>
+        <section>
+            <FaNode />
+            <h6>Node</h6>
+        </section>
+    </section>
     )
 }
 
