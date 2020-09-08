2b31c784a533c1d4c66c96188262b3d8f8928acd
Joe O <j0liverko@gmail.com>
2020-09-08 13:58:34 -0500

    added words

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,8 @@ const Home = () => {
         <section className='HomeTextContainer'>
             <section className='text'>
                 <h5>Hello my name is Joseph Oliver </h5>
-                <p>Im a person that likes to learn new languages, frameworks and new challenges. Im always consuming  <br/>
+                <p>Im a person that likes to learn new languages, frameworks and new challenges. I orignally started coding when myspace was the go to social platform and you could customize your own page, from there i my love for code grew! <br/>
+                
                     </p>
             </section>
             <SkillCard />
