31bcdd2f457a603a64fc89b8e053b56e4aa5794c
Joe O <j0liverko@gmail.com>
2020-09-08 13:21:26 -0500

    styled project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,15 +6,20 @@ import ProjectsCard from './ProjectsCard'
 
 //imgs
 import p1 from '../imgs/quality.png'
-
+import p2 from '../imgs/guidr.png'
+import p3 from '../imgs/park.png'
 
 const Projects = () => {
     
     return(
         <div className='projects'>
             <h1>whywork</h1>
-            <p>please work</p>
-            <ProjectsCard data={p1}/>
+            <section className='projectList'>
+                <ProjectsCard data={p1}/>
+                <ProjectsCard data={p2}/>
+                <ProjectsCard data={p3}/>
+            </section>
+            
         </div>
     )
 }
