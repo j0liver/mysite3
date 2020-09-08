a48dd7b9b3354b322c4e81f4e19642c87a7a8762
Joe O <j0liverko@gmail.com>
2020-09-08 14:02:58 -0500

    added descriptions to project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,9 +15,9 @@ const Projects = () => {
         <div className='projects'>
             <h1>Projects</h1>
             <section className='projectList'>
-                <ProjectsCard data={p1} name='interviewQ'/>
-                <ProjectsCard data={p2} name='Guidr'/>
-                <ProjectsCard data={p3} name='Park Passport'/>
+                <ProjectsCard data={p1} name='interviewQ' description='QualityHub is a web-primary app for both job seekers to polish their skills and for coaches to sell their expertise.'/>
+                <ProjectsCard data={p2} name='Guidr' description='Built for a 4 day challenge and designed for explorers and travellers, Guidr allows users to see fellow members trips and create trips.'/>
+                <ProjectsCard data={p3} name='Park Passport' description='Built for a 4 day challenge and designed for park lovers and travellers, Park Passport allows users to search for parks and create new posts about Parks not listed.'/>
             </section>
             
         </div>
--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -10,7 +10,7 @@ const ProjectsCards = (props) => {
                  <img src={props.data} alt=""/>
             </div>
             
-            <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <p>{props.description}</p>
             <div className='projectButton'>
                 <button>View Live</button>
                 <button>View Code</button>
