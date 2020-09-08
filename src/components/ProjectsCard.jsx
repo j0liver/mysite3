04c6af57e173c387290f23e72ffea228d3b442bf
Joe O <j0liverko@gmail.com>
2020-09-08 01:18:37 -0500

    project card layout

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -5,10 +5,16 @@ const ProjectsCards = (props) => {
 
     return(
         <section className='projectCard'>
+            <h3>test1</h3>
             <div>
                  <img src={props.data} alt=""/>
             </div>
-            <p>test1</p>
+            
+            <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <div>
+                <button>View Live</button>
+                <button>Code</button>
+            </div>
         </section>
     )
 }
